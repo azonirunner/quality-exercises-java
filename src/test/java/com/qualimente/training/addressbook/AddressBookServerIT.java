9d9c53255f818405e6089e40520f0225c05a3e1f
Stephen Kuenzli <skuenzli@qualimente.com>
2016-05-15 14:15:49 -0700

    Create an integration test that verifies the application context loads.

---
+++ src/test/java/com/qualimente/training/addressbook/AddressBookServerIT.java
@@ -0,0 +1,16 @@
+package com.qualimente.training.addressbook;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = AddressBookServer.class)
+public class AddressBookServerIT {
+
+  @Test
+  public void contextLoads() {
+  }
+
+}
