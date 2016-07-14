9795571e6f25964db76c4fc0bf7e3d8172dbe7ae
Stephen Kuenzli <skuenzli@qualimente.com>
2016-07-14 09:55:19 -0700

    Remove unused import statement.

--- src/main/java/com/qualimente/training/addressbook/LocationDataValidatorImpl.java
+++ src/main/java/com/qualimente/training/addressbook/LocationDataValidatorImpl.java
@@ -3,7 +3,6 @@ package com.qualimente.training.addressbook;
 import org.apache.commons.lang3.builder.ToStringBuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
 import java.io.BufferedReader;
