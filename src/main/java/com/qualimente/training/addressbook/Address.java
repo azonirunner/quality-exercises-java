53047457ba059d6f16ae8641399cc85aedca1fca
Stephen Kuenzli <skuenzli@qualimente.com>
2017-05-19 08:18:49 -0700

    Revert "Add JSONProperty annotation describing new 'name' property to get API tests passing again."
    This reverts commit 28acb660a2a27afe34baf160a2acac947e2c90a8.

--- src/main/java/com/qualimente/training/addressbook/Address.java
+++ src/main/java/com/qualimente/training/addressbook/Address.java
@@ -21,7 +21,7 @@ public class Address {
   private final String country;
 
   public Address(@JsonProperty("id") String id,
-                 @JsonProperty("name") String name,
+                 String name,
                  @JsonProperty("line1") String line1,
                  @JsonProperty("line2") String line2,
                  @JsonProperty("city") String city,
