c25142e42ed8460c4f0d653c770490e06e692cdf
Stephen Kuenzli <skuenzli@qualimente.com>
2017-05-24 20:05:33 -0700

    Demonstrate that requests are captured from tests.

--- src/test/java/com/qualimente/training/addressbook/AddressBookAPIHoverflyRecorderMockMvcAPITest.java
+++ src/test/java/com/qualimente/training/addressbook/AddressBookAPIHoverflyRecorderMockMvcAPITest.java
@@ -86,7 +86,7 @@ public class AddressBookAPIHoverflyRecorderMockMvcAPITest {
     String customerId = makeCustomerId();
     assertAddressBookIsNotFoundForCustomer(customerId);
 
-    Address expected = new Address(null, "Zaphod Beeblebrox", "42 Douglas Adams Way", null, "Phoenix", "85042", "AZ", "US");
+    Address expected = new Address(null, "Ford Prefect 2017-05-24", "42 Douglas Adams Way", null, "Phoenix", "85042", "AZ", "US");
 
     MvcResult result = mockMvc.perform(post(getCustomerAddressesUrl(), customerId)
         .contentType(MediaType.APPLICATION_JSON)
