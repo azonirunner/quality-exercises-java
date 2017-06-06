4f54f7cd374c87104501d58056729b0de80b777f
Stephen Kuenzli <skuenzli@qualimente.com>
2017-06-06 12:19:59 -0700

    Explore the weird case of China and the 'invalid' CN country code.

--- src/test/java/com/qualimente/training/addressbook/LocationDataValidatorTest.java
+++ src/test/java/com/qualimente/training/addressbook/LocationDataValidatorTest.java
@@ -1,5 +1,6 @@
 package com.qualimente.training.addressbook;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertFalse;
@@ -7,11 +8,27 @@ import static org.junit.Assert.assertTrue;
 
 public class LocationDataValidatorTest {
 
+  private LocationDataValidator locationDataValidator;
+
+  @Before
+  public void before(){
+    locationDataValidator = LocationDataValidator.getInstance();
+  }
+
   @Test
   public void isCountryCode_should_be_true_for_valid_countries() {
-    for (String countryCode : new String[]{"CA", "DE", "FR", "IT", "JP", "RU", "US", "GB"}) {
+    for (String countryCode : new String[]{"CA", "DE", "FR", "IT", "JP", "RU", "US", "GB", "CH"}) {
       assertTrue("expected " + countryCode + " to be valid",
-          LocationDataValidator.getInstance().isCountryCodeValid(countryCode));
+          locationDataValidator.isCountryCodeValid(countryCode));
+    }
+  }
+
+  @Test
+  public void weird_situation_where_isCountryCode_should_be_true_but_is_not() {
+    String country_code_china = "CN";
+    for (String countryCode : new String[]{country_code_china}) {
+      assertFalse("expected " + countryCode + " to be invalid",
+          locationDataValidator.isCountryCodeValid(countryCode));
     }
   }
 
@@ -19,7 +36,7 @@ public class LocationDataValidatorTest {
   public void isCountryCode_should_be_false_for_invalid_countries() {
     for (String countryCode : new String[]{"UK"}) {
       assertFalse("expected " + countryCode + " to be invalid",
-          LocationDataValidator.getInstance().isCountryCodeValid(countryCode));
+          locationDataValidator.isCountryCodeValid(countryCode));
     }
   }
 
